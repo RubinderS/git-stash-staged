c334acb75abf1d007e1034168b50a78b33aa6930
Rubinder Singh <mail2rubinder@gmail.com>
2021-08-07 01:48:04 +1000

    removed sheband-loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: [{loader: 'ts-loader'}, {loader: 'shebang-loader'}],
+        use: [{loader: 'ts-loader'}],
       },
     ],
   },
