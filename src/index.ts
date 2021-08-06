e6ffe4e2aa1d99799adb6669ba66efc3b8394fce
Rubinder Singh <mail2rubinder@gmail.com>
2021-08-06 23:00:31 +1000

    working

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import {runCommand} from './runCommand';
 
+function parseFilePathsFromDiff(diff: string): string[] {
+  const filePaths: string[] = [];
+  const regex = new RegExp(/diff --git a\/(.*)\sb\//g);
+  let regExpArr: RegExpExecArray | null;
+
+  while ((regExpArr = regex.exec(diff))) {
+    filePaths.push(regExpArr[1]);
+  }
+
+  return filePaths;
+}
+
 async function gitStashStaged(dir: string) {
   const badStashMessage = Date.now();
   const goodStashMessage = Date.now();
+
+  const currentStaged = await runCommand('git', ['diff', '--cached']);
+
+  const stagedFiles = parseFilePathsFromDiff(currentStaged);
 
   await runCommand('git', [
     'stash',
@@ -13,6 +29,12 @@ async function gitStashStaged(dir: string) {
 
   await runCommand('git', ['stash', '-m', String(goodStashMessage)]);
   await runCommand('git', ['stash', 'pop', 'stash@{1}']);
+
+  if (process.argv[2] !== '--keep') {
+    stagedFiles.forEach(async (file) => {
+      await runCommand('git', ['restore', file]);
+    });
+  }
 }
 
 gitStashStaged(process.cwd());
