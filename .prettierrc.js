27fa4692c28e83139190b05ccf478d7d4ab8d51d
Rubinder Singh <mail2rubinder@gmail.com>
2021-08-08 13:36:27 +1000

    Added eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  singleQuote: true,
+};
