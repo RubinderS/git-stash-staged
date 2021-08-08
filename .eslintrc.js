73eb4c6e3abc1fbceeb50712cd0e0ef40816361f
Rubinder Singh <mail2rubinder@gmail.com>
2021-08-08 13:45:12 +1000

    updated eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-object-literal-type-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
   },
 };
